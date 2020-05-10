52e0e4ad382fc6025fc44165abf6431cfb770017
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-10 03:54:08 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   count=0 
   
-  while count<array.length do 
-  puts array.index(value_to_find)
+  while count < array.length do 
+  puts array[count]=array.index(value_to_find)
   count+=1
   end
 end
